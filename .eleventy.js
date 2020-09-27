b1cdab376b114c98175baacd870f546c495d2fa0
R. Ilham Sastronegoro <radenpioneer@users.noreply.github.com>
2020-09-27 22:01:18 +0700

    build 27/09

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,19 @@
+const ampPlugin = require('@ampproject/eleventy-plugin-amp')
+
 module.exports = function(eleventyConfig){
+
+    //AMP Plugin
+    eleventyConfig.addPlugin(ampPlugin)
 
     //layout aliases
     eleventyConfig.addLayoutAlias('base', 'layout/base.html')
+    eleventyConfig.addLayoutAlias('layout', 'layout/layout.html')
+    eleventyConfig.addLayoutAlias('home', 'layout/home.html')
+
+    //passthrough copy assets
+    eleventyConfig.addPassthroughCopy({
+        'src/_assets': 'assets'
+    })
 
     // default i/o directory
     return {
